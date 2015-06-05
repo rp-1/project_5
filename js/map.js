3ebe31728aa2ee2e53edd66b7d72a68671952231
rp-1 <richpaschall@comcast.net>
2015-06-05 08:49:31 -0500

    before concept change

--- js/map.js
+++ js/map.js
@@ -1,39 +1,48 @@
-
+// http://jsfiddle.net/JMPerez/0u0v7e1b/
 
 function ViewModel() {
     var self = this;
-    self.places = [];
-    self.matchedPlaces = ko.observableArray([]);
+    self.places = ko.observableArray([]);
+
     self.searchString = ko.observable();    // what user entered in search field
     self.map = null;
     
+    // Called when an li item is clicked in the main places list
     self.placeListClicked = function(placeClicked) {
         console.log("Place clicked");
         console.log(placeClicked.name);
+        self.showInfo(placeClicked);
     }
 
     // called every time a character is added or edited in main search bar
     self.searchString.subscribe(function(value) {
 
         if (value !== '') {
-            self.matchedPlaces.removeAll();
-             console.log(self.places);
-            console.log("Length of self.places is " + self.places.length);
-            for (var i = 0; i < self.places.length; i++) {
+
+            for (var i = 0; i < self.places().length; i++) {
                 var search_string = value.toLowerCase();
-                var full_tag = self.places[i].name.toLowerCase();
+                var full_tag = self.places()[i].name.toLowerCase();
                 var tag_array = full_tag.split(" ");
+                var matchFound = false;
                 for(var x = 0; x < tag_array.length; x++) {
-                    console.log(tag_array[x]);
                     if( (tag_array[x].substr(0, search_string.length) == search_string)  || 
                     (full_tag.substr(0, search_string.length) == search_string) ) {
-                        self.matchedPlaces.push(self.places[i]);
+                        matchFound = true;
+                        self.places()[i].marker.setMap(self.map);
+                        self.places()[i].isActive(true);
                         break;
                     }
                 }
+                if(!matchFound) {
+                    self.places()[i].isActive(false);
+                    self.places()[i].marker.setMap(null);
+                }
             }
         } else {
-            self.matchedPlaces(self.places.slice());
+            for(var i = 0; i < self.places().length; i++) {
+                self.places()[i].marker.setMap(self.map);
+                self.places()[i].isActive(true);
+            }
         }
     });
     
@@ -47,11 +56,41 @@ function ViewModel() {
             async: true,
             dataType: "json",
             success: function(data) {
-                self.places = data.places.slice(); // must cocpy array not simply assign
-                self.matchedPlaces(data.places.slice());
+                // first create map
+                self.createMap();
+                
+                // Add two key properties to array then add each array element
+                // to self.places array. Must add these properties here, rather
+                // than in map init or things won't bind correctly. Learned the hard way.
+                for(var i = 0; i < data.places.length; i++) {
+                    var place = data.places[i];
+                    
+                    // Must make this observable to react to bool condition in html li element
+                    place.isActive = ko.observable(true);
+                    
+                    
+                    //var img = "images/icon_pic.png"; // must be location relative to index.html
+                    var marker = new google.maps.Marker({position:place.position, name:place.name, description:place.description});
+                    marker.image = place.image;
+                    marker.setMap(self.map);
+
+                    // Add event listener to capture clicks on individual markers
+                    // When clicked, each marker will show info pop up
+                    
+                    google.maps.event.addListener(marker, 'click', (function(placeCopy) {
+                        return function() {
+                            self.showInfo(placeCopy);
+                        }
+                    }) (place));
+                    
+
+                    // Make the marker an object of the place object
+                    place.marker = marker;
+                    self.places.push(data.places[i]);
+                }
                 
                 // Now that we know we have data, create the map
-                self.createMap();
+                //self.createMap();
             },
             error: function(obj, textStatus, errorThrown) {
                 // TODO: HTML RESPONSE TO USER AND RETURN
@@ -89,7 +128,7 @@ function ViewModel() {
         self.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
         
         // With map created, now we can add markers
-        self.createMarkersFromData();
+        //self.createMarkersFromData();
         
         // Resize map responsively when browser is resized.
         // http://hsmoore.com/blog/keep-google-map-v3-centered-when-browser-is-resized/
@@ -102,8 +141,9 @@ function ViewModel() {
     
     this.createMarkersFromData = function() {
 
-        for(var i = 0; i < self.places.length; i++) {
-            var place = self.places[i];
+        for(var i = 0; i < self.places().length; i++) {
+
+            var place = self.places()[i];
             //var img = "images/icon_pic.png"; // must be location relative to index.html
             var marker = new google.maps.Marker({position:place.position, name:place.name, description:place.description});
             marker.image = place.image;
@@ -116,21 +156,33 @@ function ViewModel() {
                     self.showInfo(markerCopy);
                 }
             }) (marker));
+            
+            // Make the marker an object of the place object
+            self.places()[i].marker = marker;
+            self.places()[i].isActive = ko.observable(false);
+            console.log(self.places()[i].isActive());
+            
+ 
         }
+        
         
     };
     
-    this.showInfo = function(marker) {
+    this.showInfo = function(place) {
         // Create the html content for the infoWindow
-        var markerContent = "<div class='info-window'><img class='info-img' src='" + 
-            marker.image + "'><p>" + marker.description + "</p>";
+        var mapImg = "https://maps.googleapis.com/maps/api/staticmap?center=" + place.marker.getPosition().lat() + "," + place.marker.getPosition().lng() + "&zoom=20&size=400x400&maptype=hybrid"
+        
+        var markerContent = "<div class='info-window'><h3>" + place.name + 
+            "</h3><img class='info-img' src='" + 
+            mapImg + "'><p>" + place.marker.description + "</p>";
         
         if(!self.infoWindow) {
             self.infoWindow = new google.maps.InfoWindow();
         }
         
         self.infoWindow.setContent(markerContent);
-        self.infoWindow.open(this.map, marker);
+        self.infoWindow.open(this.map, place.marker);
+
     };
     
     this.updatePlaceList = function(places2show) {
@@ -142,6 +194,7 @@ function ViewModel() {
     }
         
     self.init();
+    
 
 }
 
